83e0933e63e1bd1070e53acc0a26307877b887ab
JavaTheHuttt <tim.rademacher@kabelmail.de>
2016-07-24 20:45:20 +0200

    Fixed event loading

--- bungeeplugin/src/main/java/de/nullpointer/bungeeplugin/Main.java
+++ bungeeplugin/src/main/java/de/nullpointer/bungeeplugin/Main.java
@@ -1,18 +1,16 @@
 package de.nullpointer.bungeeplugin;
 
-import de.nullpointer.bungeeplugin.events.LoginEvent;
+import de.nullpointer.bungeeplugin.events.ConnectEvent;
+import de.nullpointer.bungeeplugin.events.DisconnectEvent;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class Main extends Plugin {
-
+	
 	@Override
 	public void onEnable() {
-		loadEvents();
+		new DisconnectEvent(this);
+		new ConnectEvent(this);
 		getLogger().info("§a" + getDescription().getName() + " on Version " + getDescription().getVersion() + " loaded!");
-	}
-
-	private void loadEvents() {
-		getProxy().getPluginManager().registerListener(this, new LoginEvent());
 	}
 
 }
